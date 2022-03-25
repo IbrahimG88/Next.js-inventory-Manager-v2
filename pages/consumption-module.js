ab1962604b392fc7f0251f8783382806b2cd8415
Ibrahim El Gohary <goharyibrahim@gmail.com>
2022-03-25 17:51:45 +0200

    paneltypes for in

--- pages/consumption-module.js
+++ pages/consumption-module.js
@@ -1,6 +1,3 @@
-import { injectStylesIntoStaticMarkup } from "@mantine/next";
-import { Suspense } from "react";
-
 export const panelTypes = [];
 
 export const getStaticProps = async () => {
@@ -23,33 +20,17 @@ const FrequencyWorklist = () => {
 
   const custFreq = panelTypes.reduce((acc, curr) => {
     acc[curr] = (acc[curr] ?? 0) + 1;
-    // if (acc[curr] >= 3) {
-    // result[curr] = acc[curr];
-    // }
-    //console.log("acc", acc); //whole list;
-    //console.log("curr", curr); //names;
-    //console.log("acc[cur]", acc[curr]); //frequency;
 
     return acc;
-
-    //return acc;
   }, {});
 
   for (const key in custFreq) {
-    console.log("key", key);
-    console.log(" custFreq[key]", custFreq[key]);
     finalArray.push({
       name: key,
       frequency: custFreq[key],
     });
   }
 
-  //console.log("custFreq", custFreq);
-  // console.log("result:", result);
-  //custFreq is what I need
-  //const custFreqArray = [];
-  //custFreqArray.push(custFreq);
-  // console.log("custFreqArray", custFreqArray);
   return (
     <ul>
       {finalArray.map((item) => (
@@ -62,24 +43,13 @@ const FrequencyWorklist = () => {
 };
 
 const OccurrancestrialsPage = ({ res }) => {
-  //  console.log("res", res);
+  for (const key in res) {
+    for (const myItem in res[key].panels) {
+      panelTypes.push(res[key].panels[myItem].report_name);
+    }
+  }
+  console.log("panelTypes", panelTypes);
 
-  return (
-    <>
-      <div>
-        <ul>
-          {res.map((item) => {
-            item.panels.map((occurrances) => {
-              panelTypes.push(occurrances.report_name);
-              return panelTypes;
-            });
-          })}
-        </ul>
-        <FrequencyWorklist />
-      </div>
-    </>
-  );
+  return <FrequencyWorklist />;
 };
 export default OccurrancestrialsPage;
-
-// meed to only add the final occurrance create and not by each count added
