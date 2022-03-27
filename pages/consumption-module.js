fb9d2696466dd6352ea14a74ad4b7807cba2af54
Ibrahim El Gohary <goharyibrahim@gmail.com>
2022-03-27 15:05:52 +0200

    date calculation and details done

--- pages/consumption-module.js
+++ pages/consumption-module.js
@@ -1,8 +1,52 @@
+import { useState } from "react";
+
 export const panelTypes = [];
 
 export const getStaticProps = async () => {
+  // need to get the date from database item here:
+  const data = await fetch("http://localhost:3000/api/item/1").then(
+    (response) => {
+      return response.json().then((data) => {
+        console.log("data look", data);
+        for (const key in data) {
+          const myDate = new Date(data[key].date);
+          return myDate;
+        }
+        console.log("my date look", myDate);
+        return myDate;
+      });
+    }
+  );
+  console.log("myData", data);
+
+  const dateIndividualData = (singleDate) => {
+    const dateObject = {
+      day: singleDate.getDate(),
+      month: singleDate.getMonth() + 1,
+      year: singleDate.getFullYear(),
+    };
+
+    return dateObject;
+  };
+
+  const nowDate = () => {
+    const now = new Date();
+    const dateObject = {
+      day: now.getDate(),
+      month: now.getMonth() + 1,
+      year: now.getFullYear(),
+    };
+    console.log("today", dateObject.day);
+    return dateObject;
+  };
+
+  const dateSample = dateIndividualData(data);
   const res = await fetch(
-    "http://197.45.107.206/api2/integration/worklist/2021-12-02%2000:00:00:00/2021-12-05%2000:00:00:00"
+    `http://197.45.107.206/api2/integration/worklist/${dateSample.year}-${
+      dateSample.month
+    }-${dateSample.day}%2000:00:00:00/${nowDate().year}-${nowDate().month}-${
+      nowDate().day
+    }%2000:00:00:00`
   ).then((response) => {
     return response.json().then((data) => {
       return data;
@@ -52,3 +96,5 @@ const OccurrancestrialsPage = ({ res }) => {
   return <FrequencyWorklist />;
 };
 export default OccurrancestrialsPage;
+
+// now get the date form the database
