2075666bc5d5cedd06112f558e246a40b33901ef
Ibrahim El Gohary <goharyibrahim@gmail.com>
2022-03-31 18:56:07 +0200

    upsert tests only if not exist

---
+++ pages/api/api-insert-test-mongo-check-first.js
@@ -0,0 +1,24 @@
+import { connectDatabase } from "../../helpers/db-utils";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const client = await connectDatabase();
+    const db = client.db();
+    const testsList = req.body;
+
+    for (const key in testsList) {
+      const addItem = await db
+        .collection("inventory")
+        .updateOne(
+          { testName: testsList[key].name },
+          {
+            $set: { id: testsList[key].id, testName: testsList[key].testName },
+          },
+          { upsert: true }
+        );
+    }
+  }
+  res.status(201).json({ message: "All Items Saved" });
+}
+
+export default handler;
